e9fdc04504391d2539689f817c0a178f4e58d840
Vladnickgo <vladnickgo@gmail.com>
2023-05-28 19:10:47 +0300

    Add TagService

--- src/main/java/com/epam/esm/SpringApiAdvanced/repository/entity/User.java
+++ src/main/java/com/epam/esm/SpringApiAdvanced/repository/entity/User.java
@@ -7,8 +7,8 @@ import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
-//@Getter
-//@Setter
+@Getter
+@Setter
 @EqualsAndHashCode
 @ToString
 @Builder
@@ -22,44 +22,4 @@ public class User implements Serializable {
     private String lastName;
     private String email;
     private String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
